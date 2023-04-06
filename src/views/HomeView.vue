e37f1b3b3b2e7aa795e1dadb8c12b6aabd1cb0f6
freddy haryono <freddyharyononoctis@gmail.com>
2023-04-06 07:39:26 +0700

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <PostList v-if="showPosts" :posts="posts"/>
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
+  </div>
+</template>
+
+<script>
+import PostList from '@/components/PostList.vue';
+import { ref } from 'vue';
+
+
+export default {
+  name: 'HomeView',
+  components: { PostList },
+  setup(){
+    const posts = ref([])
+    const error = ref(null)
+
+    const load = async () => {
+      try{
+        let data = await fetch('http://localhost:3000/posts')
+        if(!data.ok){
+          throw Error('no data')
+        }
+      }
+      catch(err) {
+        error.value = err.message
+        console.log(err.value)
+      }
+    }
+
+    load()
+    return { posts, showPosts } 
+  }
+}
+</script>
